771be85150804dbfbff1fc23e0f9f0a6b5e5cbef
Paul Edwards <paul.m.edwards@gmail.com>
2021-10-01 11:40:36 +0100

    removed yum install for hsm (contained in image)

--- scripts/lfshsm.sh
+++ scripts/lfshsm.sh
@@ -9,12 +9,6 @@ storage_account="$2"
 storage_sas="$3"
 storage_container="$4"
 
-if ! rpm -q lemur-azure-hsm-agent lemur-azure-data-movers; then
-    yum -y install \
-        https://azurehpc.azureedge.net/rpms/lemur-azure-hsm-agent-2.0.1-lustre_2.12.x86_64.rpm \
-        https://azurehpc.azureedge.net/rpms/lemur-azure-data-movers-2.0.1-lustre_2.12.x86_64.rpm
-fi
-
 mkdir -p /var/run/lhsmd
 chmod 755 /var/run/lhsmd
 
