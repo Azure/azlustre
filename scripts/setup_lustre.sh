24e0514842e6558ef60faf0bb0eb23227b58c939
Paul Edwards <paul.m.edwards@gmail.com>
2021-11-26 18:36:28 +0000

    First implementation - untested

--- scripts/setup_lustre.sh
+++ scripts/setup_lustre.sh
@@ -15,6 +15,7 @@ log_analytics_name="$6"
 log_analytics_workspace_id="$7"
 log_analytics_key="$8"
 oss_disk_setup="$9"
+deploy_policy_engine="${10}"
 
 echo mds="$1"
 echo n_oss="$2"
@@ -26,24 +27,7 @@ echo log_analytics_workspace_id="$7"
 echo log_analytics_key="$8"
 echo oss_disk_setup="$9"
 
-
-# vars used in script
-if [ -e /dev/nvme0n1 ]; then
-	devices='/dev/nvme*n1'
-	n_devices=$(echo $devices | wc -w)
-	echo "Using $n_devices NVME devices"
-elif [ -e /dev/sdc ]; then
-	devices='/dev/sd[c-m]'
-	n_devices=$(echo $devices | wc -w)
-	echo "Using $n_devices NVME devices"
-elif [ -e /dev/sdb ]; then
-	devices='/dev/sdb'
-	n_devices=1
-	echo "Using ephemeral disk on /dev/sdb"
-else
-	echo "ERROR: cannot find devices for storage"
-	exit 1
-fi
+rbh="${mds}rbh"
 
 lustre_version=2.12.5
 
@@ -59,24 +43,7 @@ else
 	use_log_analytics=true
 fi
 
-if [[ "$n_devices" -gt "1" && ( "$oss_disk_setup" = "raid" || "$HOSTNAME" = "$mds" ) ]]; then
-	device=/dev/md10
-	echo "creating raid ($device) from $n_devices devices : $devices"
-	$script_dir/create_raid0.sh $device $devices
-	devices=$device
-	n_devices=1
-fi
-
-echo "using $n_devices device(s) : $devices"
-
-ost_index=1
-
-if [ "$HOSTNAME" = "$mds" ]; then
-
-	# SETUP MDS
-	$script_dir/lfsmaster.sh $devices $n_oss
-
-else
+if [ "$HOSTNAME" = "$rbh" ]; then
 
 	echo "wait for the mds to start"
 	modprobe lustre
@@ -84,25 +51,70 @@ else
 		sleep 2
 	done
 
+else 
 
-	idx=0
-	for c in $(echo ${HOSTNAME##$mds} | grep -o .); do
-		echo $c		
-		idx=$(($idx * 36))
-		if [ -z "${c##[0-9]}" ]; then
-			idx=$(($idx + $c))
-		else
-			idx=$(($(printf "$idx + 10 + %d - %d" "'${c^^}" "'A")))
-		fi
-	done
-	
-	ost_index=$(( ( $idx * $n_devices ) + 1 ))
+	# vars used in script
+	if [ -e /dev/nvme0n1 ]; then
+		devices='/dev/nvme*n1'
+		n_devices=$(echo $devices | wc -w)
+		echo "Using $n_devices NVME devices"
+	elif [ -e /dev/sdc ]; then
+		devices='/dev/sd[c-m]'
+		n_devices=$(echo $devices | wc -w)
+		echo "Using $n_devices NVME devices"
+	elif [ -e /dev/sdb ]; then
+		devices='/dev/sdb'
+		n_devices=1
+		echo "Using ephemeral disk on /dev/sdb"
+	else
+		echo "ERROR: cannot find devices for storage"
+		exit 1
+	fi
 
-	echo "starting ost index=$ost_index"
+	if [[ "$n_devices" -gt "1" && ( "$oss_disk_setup" = "raid" || "$HOSTNAME" = "$mds" ) ]]; then
+		device=/dev/md10
+		echo "creating raid ($device) from $n_devices devices : $devices"
+		$script_dir/create_raid0.sh $device $devices
+		devices=$device
+		n_devices=1
+	fi
 
-	mds_ip=$(ping -c 1 $mds | head -1 | sed 's/^[^)]*(//g;s/).*$//g')
+	echo "using $n_devices device(s) : $devices"
 
-	$script_dir/lfsoss.sh $mds_ip "$devices" $ost_index
+
+	if [ "$HOSTNAME" = "$mds" ]; then
+
+		# SETUP MDS
+		$script_dir/lfsmaster.sh $devices $n_oss
+
+	else
+
+		echo "wait for the mds to start"
+		modprobe lustre
+		while ! lctl ping $mds@tcp; do
+			sleep 2
+		done
+
+		idx=0
+		for c in $(echo ${HOSTNAME##$mds} | grep -o .); do
+			echo $c		
+			idx=$(($idx * 36))
+			if [ -z "${c##[0-9]}" ]; then
+				idx=$(($idx + $c))
+			else
+				idx=$(($(printf "$idx + 10 + %d - %d" "'${c^^}" "'A")))
+			fi
+		done
+		
+		ost_index=$(( ( $idx * $n_devices ) + 1 ))
+
+		echo "starting ost index=$ost_index"
+
+		mds_ip=$(ping -c 1 $mds | head -1 | sed 's/^[^)]*(//g;s/).*$//g')
+
+		$script_dir/lfsoss.sh $mds_ip "$devices" $ost_index
+
+	fi
 
 fi
 
@@ -110,12 +122,36 @@ if [ "${use_hsm,,}" = "true" ]; then
 
 	if [ "$HOSTNAME" = "$mds" ]; then
 
-		# IMPORT CONTAINER (so mount lustre on the mds to import)
-		mkdir /lustre
-		mount -t lustre ${mds}@tcp0:/LustreFS /lustre
-		chmod 777 /lustre
+		# deploy on the MDS if we aren't using a policy engine
+		if [ "$deploy_policy_engine" = "True" ]; then
 
+			# register cl1
+			lfs --device LustreFS-MDT0000 changelog_register
+			# register cl2
+			lfs --device LustreFS-MDT0000 changelog_register
+
+		else
+
+			# IMPORT CONTAINER (so mount lustre on the mds to import)
+			mkdir /lustre
+			mount -t lustre ${mds}@tcp0:/LustreFS /lustre
+			chmod 777 /lustre
+
+			$script_dir/lfsimport.sh "$storage_account" "$storage_sas" "$storage_container" /lustre
+		
+		fi
+
+	elif [ "$HOSTNAME" = "${rbh}" ]; then
+
+		# IMPORT CONTAINER
+		mkdir /lustre
+		echo "${mds}@tcp0:/LustreFS $lfs_mount lustre flock,defaults,_netdev 0 0" >> /etc/fstab
+		mount -a
+		chmod 777 /lustre
+		
 		$script_dir/lfsimport.sh "$storage_account" "$storage_sas" "$storage_container" /lustre
+		
+		$script_dir/lfsrbh.sh $mds "$storage_account" "$storage_sas" "$storage_container"
 
 	else
 
